0332d6c7fa20392f12b98dc198a1ae7f9d3634f7
oliver-breen <oliver.breen000@gmail.com>
2026-02-13 21:11:45 -0700

    Fix Windows C++20 build

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import Extension, setup
-from setuptools.command.build_ext import build_ext
+from setuptools.command.build_ext import build_ext as _build_ext
 
 import pybind11
+
 
 falcon_extension = Extension(
     "quantaweave._falcon",
     sources=[
         "quantaweave/_falcon_bindings.cpp",
-        "vendor/hqc/lib/fips202/fips202.c",
+        "quantaweave/_fips202_wrapper.cpp",
     ],
     include_dirs=[
         pybind11.get_include(),
+        pybind11.get_include(True),
         "vendor/falcon/include",
         "vendor/hqc/lib/fips202",
     ],
@@ -20,56 +22,20 @@ falcon_extension = Extension(
 )
 
 
-class BuildExt(build_ext):
+class BuildExt(_build_ext):
     def build_extension(self, ext):
-        if self.compiler is None:
-            return super().build_extension(ext)
+        compiler_type = getattr(self.compiler, "compiler_type", "")
+        args = list(getattr(ext, "extra_compile_args", []) or [])
+        if compiler_type == "msvc":
+            args = [arg for arg in args if not arg.startswith("-std")]
+            if not any(arg.startswith("/std:") for arg in args):
+                args.append("/std:c++20")
+        else:
+            args = [arg for arg in args if not arg.startswith("/std")]
+            if not any(arg.startswith("-std=") for arg in args):
+                args.append("-std=c++20")
+        ext.extra_compile_args = args
+        super().build_extension(ext)
 
-        c_sources = [src for src in ext.sources if src.endswith(".c")]
-        cxx_sources = [src for src in ext.sources if not src.endswith(".c")]
-        extra_args = list(ext.extra_compile_args or [])
-        cxx_args = extra_args
-        c_args = [arg for arg in extra_args if arg != "-std=c++20"]
 
-        objects = []
-        if c_sources:
-            objects.extend(
-                self.compiler.compile(
-                    c_sources,
-                    output_dir=self.build_temp,
-                    macros=ext.define_macros,
-                    include_dirs=ext.include_dirs,
-                    debug=self.debug,
-                    extra_postargs=c_args,
-                    depends=ext.depends,
-                )
-            )
-
-        if cxx_sources:
-            objects.extend(
-                self.compiler.compile(
-                    cxx_sources,
-                    output_dir=self.build_temp,
-                    macros=ext.define_macros,
-                    include_dirs=ext.include_dirs,
-                    debug=self.debug,
-                    extra_postargs=cxx_args,
-                    depends=ext.depends,
-                )
-            )
-
-        self.compiler.link_shared_object(
-            objects,
-            self.get_ext_fullpath(ext.name),
-            libraries=ext.libraries,
-            library_dirs=ext.library_dirs,
-            runtime_library_dirs=ext.runtime_library_dirs,
-            extra_postargs=ext.extra_link_args,
-            export_symbols=self.get_export_symbols(ext),
-            debug=self.debug,
-        )
-
-setup(
-    ext_modules=[falcon_extension],
-    cmdclass={"build_ext": BuildExt},
-)
+setup(ext_modules=[falcon_extension], cmdclass={"build_ext": BuildExt})
