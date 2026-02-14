3d78e007798749f267b2e491fd279ac37a833d65
oliver-breen <oliver.breen000@gmail.com>
2026-02-13 21:17:59 -0700

    Install GMP dependency on CI

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+import os
+
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext as _build_ext
 
 import pybind11
 
+
+gmp_include = os.environ.get("GMP_INCLUDE_DIR")
+gmp_lib = os.environ.get("GMP_LIB_DIR")
+
+include_dirs = [
+    pybind11.get_include(),
+    pybind11.get_include(True),
+    "vendor/falcon/include",
+    "vendor/hqc/lib/fips202",
+]
+if gmp_include:
+    include_dirs.append(gmp_include)
+
+library_dirs = []
+if gmp_lib:
+    library_dirs.append(gmp_lib)
 
 falcon_extension = Extension(
     "quantaweave._falcon",
@@ -10,12 +28,8 @@ falcon_extension = Extension(
         "quantaweave/_falcon_bindings.cpp",
         "quantaweave/_fips202_wrapper.cpp",
     ],
-    include_dirs=[
-        pybind11.get_include(),
-        pybind11.get_include(True),
-        "vendor/falcon/include",
-        "vendor/hqc/lib/fips202",
-    ],
+    include_dirs=include_dirs,
+    library_dirs=library_dirs,
     language="c++",
     extra_compile_args=["-std=c++20"],
     libraries=["gmp", "gmpxx"],
@@ -30,6 +44,8 @@ class BuildExt(_build_ext):
             args = [arg for arg in args if not arg.startswith("-std")]
             if not any(arg.startswith("/std:") for arg in args):
                 args.append("/std:c++20")
+            if "/utf-8" not in args:
+                args.append("/utf-8")
         else:
             args = [arg for arg in args if not arg.startswith("/std")]
             if not any(arg.startswith("-std=") for arg in args):
